89ceb488f4fb088faf9d58500a78ba0dc5c8808c
JY2024 <92546705+JY2024@users.noreply.github.com>
2022-06-29 17:42:30 -0400

    Fixed walking animation

---
+++ Nut.gd
@@ -0,0 +1,19 @@
+extends Sprite
+
+signal picked_up
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	pass
+
+func _on_CannotPickUpTimer_timeout():
+	# self.set_texture()
+	pass
+
+
+func _on_GrowIntoTreeTimer_timeout():
+	# self.set_texture()
+	pass
