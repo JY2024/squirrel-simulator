b9a544940d563f7bba7e270534a4179b6b532183
JY2024 <92546705+JY2024@users.noreply.github.com>
2022-07-11 18:40:31 -0400

    Fixed nut scaling issue

--- NutHolder.gd
+++ NutHolder.gd
@@ -5,16 +5,22 @@ class_name NutHolder
 signal picked_up
 
 var pickup_available = false
-var pos_x
-var pos_y
 
 func _init(position_x, position_y):
-	pos_x = position_x
-	pos_y = position_y
+	# Create and resize texture from dirt patch image
+	var img = Image.new()
+	img.load("res://art/dirt_patch.png")
+#	# Set texture
+	var my_texture = ImageTexture.new()
+	my_texture.create_from_image(img)
+	self.set_texture(my_texture)
+	# Set position
+	position.x = position_x
+	position.y = position_y
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 func _process(delta):
 	pass
