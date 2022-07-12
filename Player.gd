38a47189aed5e30ed5f6a0437fdf0793525c57df
JY2024 <92546705+JY2024@users.noreply.github.com>
2022-07-12 18:13:46 -0400

    bullet shoots out

--- Player.gd
+++ Player.gd
@@ -1,6 +1,9 @@
 extends KinematicBody2D
 
 export var speed = 400
+export (PackedScene) var Bullet
+
+onready var end_of_gun = $EndOfGun
 var screen_size
 
 # Runs upon entering the scene tree
@@ -30,10 +33,23 @@ func _walk(delta):
 		$AnimatedSprite.stop()
 		# Update player position
 	move_and_collide(velocity)
-#	position += velocity * delta
-#	position.x = clamp(position.x, 0, screen_size.x)
-#	position.y = clamp(position.y, 0, screen_size.y)
 	
 	if velocity.x != 0:
 		$AnimatedSprite.animation = "default"
 		$AnimatedSprite.flip_h = velocity.x < 0 # Flip image
+
+func _unhandled_input(event):
+	if event.is_action_pressed("shoot"):
+		_shoot()
+
+func _shoot():
+	var this_bullet = Bullet.instance()
+	add_child(this_bullet)
+	this_bullet.global_position = end_of_gun.global_position
+	var target
+	if $AnimatedSprite.flip_h == true:
+		target = Vector2(this_bullet.global_position.x - 20, this_bullet.global_position.y)
+	else:
+		target = Vector2(this_bullet.global_position.x + 20, this_bullet.global_position.y)
+	var direction = this_bullet.global_position.direction_to(target).normalized()
+	this_bullet._set_direction(direction)
